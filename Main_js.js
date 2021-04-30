c38aff0b45ef96c717c8a3bcbeb54c2a28c63065
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-05-01 01:22:14 +0300

    api integration 1st test

--- Main_js.js
+++ Main_js.js
@@ -13,39 +13,44 @@ const h_page = $(`#home_page`);
 const a_page = $(`#airing_page`);
 const m_page = $(`#movies_page`);
 const log_reg = $(`#log_reg`);
+const info_page = $(`#info_page`)
 const new_acc = $(`#new_a`)
 const log_button = $(`#log_button`)
 const reg_button = $(`#reg_button`)
+const panel = $(`.overview`)
 
 
 home.on(`click`, () => {
-    h_page.css(`display`, `block`)
-    a_page.css(`display`, `none`)
-    m_page.css(`display`, `none`)
-    log_reg.css(`display`, `none`)
-    sessionStorage.setItem(`lastvisted` ,`h_page`)
+    h_page.css(`display`, `block`);
+    a_page.css(`display`, `none`);
+    m_page.css(`display`, `none`);
+    log_reg.css(`display`, `none`);
+    info_page.css(`display`, `none`);
+    sessionStorage.setItem(`lastvisted` ,`h_page`);
 });
 airing.on(`click`, () => {
-    h_page.css(`display`, `none`)
-    a_page.css(`display`, `block`)
-    m_page.css(`display`, `none`)
-    log_reg.css(`display`, `none`)
-    sessionStorage.setItem(`lastvisted` ,`a_page`)
+    h_page.css(`display`, `none`);
+    a_page.css(`display`, `block`);
+    m_page.css(`display`, `none`);
+    log_reg.css(`display`, `none`);
+    info_page.css(`display`, `none`);
+    sessionStorage.setItem(`lastvisted` ,`a_page`);
 });
 movies.on(`click`, () => {
-    h_page.css(`display`, `none`)
-    a_page.css(`display`, `none`)
-    m_page.css(`display`, `block`)
-    log_reg.css(`display`, `none`)
-    sessionStorage.setItem(`lastvisted` ,`m_page`)
+    h_page.css(`display`, `none`);
+    a_page.css(`display`, `none`);
+    m_page.css(`display`, `block`);
+    log_reg.css(`display`, `none`);
+    info_page.css(`display`, `none`);
+    sessionStorage.setItem(`lastvisted` ,`m_page`);
 });
 login1.on(`click`, () => {
-    h_page.css(`display`, `none`)
-    a_page.css(`display`, `none`)
-    m_page.css(`display`, `none`)
-    log_reg.css(`display`, `block`)
-    $(`#log1`).css(`display`, `block`)
-    $(`#reg`).css(`display`, `none`)
+    h_page.css(`display`, `none`);
+    a_page.css(`display`, `none`);
+    m_page.css(`display`, `none`);
+    log_reg.css(`display`, `block`);
+    $(`#log1`).css(`display`, `block`);
+    $(`#reg`).css(`display`, `none`);
 });
 new_acc.on(`click`, () => {
     $(`#log1`).css(`display`, `none`);
@@ -61,6 +66,7 @@ log_button.on(`click`, () => {
         // let welcomeText = `welcome ${user}`
         login1.css(`display`, `none`);
         loggedIn.text(`welcome ${user}`);
+        loggedIn.css(`display`, `block`);
         signOut.css(`display`, `block`);
         sessionStorage.setItem(`loggedIn`, `true`);
         log_reg.css(`display`, `none`)
@@ -92,6 +98,7 @@ reg_button.on(`click`, () => {
             let lastVisited = sessionStorage.getItem(`lastvisted`);
             // let welcomeText1 = `welcome` + user;
             loggedIn.text(`welcome ${user}`);
+            loggedIn.css(`display`, `block`);
             login1.css(`display`, `none`);
             signOut.css(`display`, `block`);
             sessionStorage.setItem(`loggedIn`, `true`)
@@ -125,8 +132,31 @@ signOut.on(`click`, () => {
     }
 })
 
+// just for testing purposes
 $.getJSON(
     `https://kitsu.io/api/edge/anime/1376`, (data) => {
         console.log(data);
     }
 )
+
+panel.on(`click`, () => {
+    info_page.css(`display`, `block`);
+    h_page.css(`display`, `none`);
+    a_page.css(`display`, `none`);
+    m_page.css(`display`, `none`);
+    $.getJSON(
+        `https://kitsu.io/api/edge/anime/1376`, (data) => {
+            $(`#poster1`).attrib(`src`, data.attributes.posterImage.original)
+            $(`#ani_title`).text(data.attributes.canonicalTitle);
+            $(`#rating`).text(data.attributes.averageRating);
+            $(`#eps`).text(data.attributes.episodeCount);
+            $(`#s_date`).text(data.attributes.startDate);
+            $(`#e_date`).text(data.attributes.endDate);
+            $(`#ani_desc`).text(data.attributes.description);
+            // $(`#`)
+            // $(`#`)
+            // $(`#`)
+            // $(`#`)
+
+        })
+})
