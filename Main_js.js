17e83cbf566feee22e2650c3eb23831fe1556bad
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-05-03 00:16:25 +0300

    explore and favourite buttons added with cycling func

--- Main_js.js
+++ Main_js.js
@@ -1,5 +1,4 @@
 const accounts = window.localStorage;
-const favourite = window.localStorage;
 sessionStorage.setItem(`loggedIn`, `false`)
 sessionStorage.setItem(`lastvisted` ,`h_page`)
 const showsIds = {death:`1376`, aot:`7442`, dragon:`720`, piece:`12`, naruto:`11`, conan:`210`, mushoku:`42323`, dunk:`148`, haikyuu:`8133`, yugi:`442`, academia:`13881`, jujutsu:`42765`, eva:`21`, punch:`10740`, steins:`5646`, rezero:`11209`, stone:`42080`, jojo:`7158`, kimetsu:`41370`, kaguya:`41373`, bunny:`41056`, boku_s5:`43108`, immortal:`43211`, higehiro:`42923`, revengers:`43321`, 86:`43066`, shaman:`43222`, vivy:`43933`, combatant:`43837`, dynazenon:`42888`, voice:`10028`, kimi:`11614`, spirited:`176`, mononoke:`142`, pancreas:`13723`,weather:`42028`, akira:`29`, garden:`7515`, centimeters:`1517` }
@@ -8,12 +7,16 @@ localStorage.setItem(`currentColor`, `blue`)
 const home = $(`#home`);
 const airing = $(`#airing`);
 const movies = $(`#movies`);
+const explore = $(`#explore`);
+const favourite = $(`#favourites`);
 const login1 = $(`#login1`);
 const loggedIn = $(`#loggedIn`)
 const signOut = $(`#login2`);
 const h_page = $(`#home_page`);
 const a_page = $(`#airing_page`);
 const m_page = $(`#movies_page`);
+const e_page = $(`#explore_page`);
+const f_page = $(`#favourites_page`);
 const log_reg = $(`#log_reg`);
 const info_page = $(`#info_page`)
 const new_acc = $(`#new_a`)
@@ -22,10 +25,13 @@ const reg_button = $(`#reg_button`)
 const panel = $(`.overview`)
 
 
+// cycling between pages
 home.on(`click`, () => {
     h_page.css(`display`, `block`);
     a_page.css(`display`, `none`);
     m_page.css(`display`, `none`);
+    e_page.css(`display`, `none`);
+    f_page.css(`display`, `none`);
     log_reg.css(`display`, `none`);
     info_page.css(`display`, `none`);
     sessionStorage.setItem(`lastvisted` ,`h_page`);
@@ -34,6 +40,8 @@ airing.on(`click`, () => {
     h_page.css(`display`, `none`);
     a_page.css(`display`, `block`);
     m_page.css(`display`, `none`);
+    e_page.css(`display`, `none`);
+    f_page.css(`display`, `none`);
     log_reg.css(`display`, `none`);
     info_page.css(`display`, `none`);
     sessionStorage.setItem(`lastvisted` ,`a_page`);
@@ -42,6 +50,28 @@ movies.on(`click`, () => {
     h_page.css(`display`, `none`);
     a_page.css(`display`, `none`);
     m_page.css(`display`, `block`);
+    e_page.css(`display`, `none`);
+    f_page.css(`display`, `none`);
+    log_reg.css(`display`, `none`);
+    info_page.css(`display`, `none`);
+    sessionStorage.setItem(`lastvisted` ,`m_page`);
+});
+explore.on(`click`, () => {
+    h_page.css(`display`, `none`);
+    a_page.css(`display`, `none`);
+    m_page.css(`display`, `none`);
+    e_page.css(`display`, `block`);
+    f_page.css(`display`, `none`);
+    log_reg.css(`display`, `none`);
+    info_page.css(`display`, `none`);
+    sessionStorage.setItem(`lastvisted` ,`m_page`);
+});
+favourite.on(`click`, () => {
+    h_page.css(`display`, `none`);
+    a_page.css(`display`, `none`);
+    m_page.css(`display`, `none`);
+    e_page.css(`display`, `none`);
+    f_page.css(`display`, `block`);
     log_reg.css(`display`, `none`);
     info_page.css(`display`, `none`);
     sessionStorage.setItem(`lastvisted` ,`m_page`);
@@ -50,22 +80,28 @@ login1.on(`click`, () => {
     h_page.css(`display`, `none`);
     a_page.css(`display`, `none`);
     m_page.css(`display`, `none`);
+    e_page.css(`display`, `none`);
+    f_page.css(`display`, `none`);
     log_reg.css(`display`, `block`);
+    info_page.css(`display`, `none`);
     $(`#log1`).css(`display`, `block`);
     $(`#reg`).css(`display`, `none`);
 });
+
+
+
+// log in function
 new_acc.on(`click`, () => {
     $(`#log1`).css(`display`, `none`);
     $(`#reg`).css(`display`, `block`);
 });
+
 log_button.on(`click`, () => {
     const user = $(`#l_user`).val();
     console.log(user);
     const pass = $(`#l_pass`).val();
     console.log(pass);
     if (accounts[user] === pass){
-        // loggedIn = true
-        // let welcomeText = `welcome ${user}`
         login1.css(`display`, `none`);
         loggedIn.text(`welcome ${user}`);
         loggedIn.css(`display`, `block`);
@@ -86,6 +122,7 @@ log_button.on(`click`, () => {
     }
 });
 
+// register function
 reg_button.on(`click`, () => {
     const user = $(`#r_user`).val();
     console.log(user);
@@ -119,6 +156,7 @@ reg_button.on(`click`, () => {
     }
 });
 
+//sign out function
 signOut.on(`click`, () => {
     login1.css(`display`, `block`);
     loggedIn.text(``);
@@ -136,11 +174,12 @@ signOut.on(`click`, () => {
 
 // just for testing purposes
 $.getJSON(
-    `https://kitsu.io/api/edge/anime/1376`, (data) => {
+    `https://kitsu.io/api/edge/anime?page[limit]=5&page[offset]=0`, (data) => {
         console.log(data);
     }
 )
 
+// going to info page
 const transition = (clickedId) => {
     info_page.css(`display`, `block`);
     h_page.css(`display`, `none`);
@@ -170,31 +209,45 @@ const transition = (clickedId) => {
         })
 }
 
-$(`#home`).on(`mouseover`,() => {
-    $(`#home`).animate({fontSize:28},`linear`);
+// effects for hovering on nav. bar
+home.on(`mouseover`,() => {
+    home.animate({fontSize:28},`linear`);
 });
-$(`#home`).on(`mouseout`,() => {
-    $(`#home`).animate({fontSize:20},`linear`);
+home.on(`mouseout`,() => {
+    home.animate({fontSize:20},`linear`);
 });
-$(`#airing`).on(`mouseover`,() => {
-    $(`#airing`).animate({fontSize:28},`linear`);
+airing.on(`mouseover`,() => {
+    airing.animate({fontSize:28},`linear`);
 });
-$(`#airing`).on(`mouseout`,() => {
-    $(`#airing`).animate({fontSize:20},`linear`);
+airing.on(`mouseout`,() => {
+    airing.animate({fontSize:20},`linear`);
 });
-$(`#movies`).on(`mouseover`,() => {
-    $(`#movies`).animate({fontSize:28},`linear`);
+movies.on(`mouseover`,() => {
+    movies.animate({fontSize:28},`linear`);
 });
-$(`#movies`).on(`mouseout`,() => {
-    $(`#movies`).animate({fontSize:20},`linear`);
+movies.on(`mouseout`,() => {
+    movies.animate({fontSize:20},`linear`);
 });
-$(`#login1`).on(`mouseover`,() => {
-    $(`#login1`).animate({fontSize:28},`linear`);
+explore.on(`mouseover`,() => {
+    explore.animate({fontSize:28},`linear`);
 });
-$(`#login1`).on(`mouseout`,() => {
-    $(`#login1`).animate({fontSize:20},`linear`);
+explore.on(`mouseout`,() => {
+    explore.animate({fontSize:20},`linear`);
+});
+favourite.on(`mouseover`,() => {
+    favourite.animate({fontSize:28},`linear`);
+});
+favourite.on(`mouseout`,() => {
+    favourite.animate({fontSize:20},`linear`);
+});
+login1.on(`mouseover`,() => {
+    login1.animate({fontSize:28},`linear`);
+});
+login1.on(`mouseout`,() => {
+    login1.animate({fontSize:20},`linear`);
 });
 
+// changing color theme
 $(`#changeColor`).on(`click`, () => {
     const current = localStorage.getItem(`currentColor`)
     if (current === `blue`){
@@ -218,7 +271,7 @@ $(`#changeColor`).on(`click`, () => {
             $(`.confirm`).css(`color`, `#000000`);
             $(`.confirm`).css(`background-color`, `#fca311`);
         })
-        $(`.insText`).css(`color`, `#ffffff`);
+        $(`.insText`).css(`color`, `#fca311`);
         $(`.colorScheme3`).css(`color`, `#ffffff`);
         $(`.colorScheme4`).css(`background-color`, `#fca311`);
         $(`.colorScheme5`).css(`border`, `solid #fca311 2px`);
@@ -258,3 +311,4 @@ $(`#changeColor`).on(`click`, () => {
         localStorage.setItem(`currentColor`, `blue`)
     }
 })
+
