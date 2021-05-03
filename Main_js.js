ec958008705950595d01227ebfae50dba6a2ce0d
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-05-03 21:34:13 +0300

    additions to login func and part of favourite add func'

--- Main_js.js
+++ Main_js.js
@@ -114,6 +114,8 @@ log_button.on(`click`, () => {
         log_reg.css(`display`, `none`)
         let lastVisited = sessionStorage.getItem(`lastvisted`);
         sessionStorage.setItem(`loggedIn`, `true`)
+        sessionStorage.setItem(`currentUser`, `${user}`)
+        favourite.css(`display`, `block`)
             if (lastVisited === `h_page`){
                 h_page.css(`display`, `block`)
             } else if (lastVisited === `a_page`){
@@ -370,7 +372,7 @@ const pageCycle = (api_link) => {
             for (let x in data.data){
                 $(`#e_main`).append(
                     ($(`<div>`).prop({
-                        id: data.data[x].attributes.id,
+                        id: data.data[x].id,
                         className: `overview2 colorScheme2 colorScheme3`
                     }).append($(`<img>`).prop({
                         src : data.data[x].attributes.posterImage.original ,
@@ -436,3 +438,14 @@ add_fav.on(`mouseout`, () => {
     <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
   </svg> favourite`)
 })
+
+// adding to favoutrites
+const favourtieAdd = (k) => {
+    let logCheck = sessionStorage.getItem(`loggedIn`)
+    if (logCheck === `true`){
+        const user = sessionStorage.getItem(`currentUser`)
+        const favouriteList = localStorage.getItem(user)
+        favouriteList.push(data.data[k].id)
+        localStorage.setItem(user, user.push())
+    } else{}
+}
