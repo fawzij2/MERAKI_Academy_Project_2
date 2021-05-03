bfbd8f0e1a1f96f26a4f828e12db30bb8fd97244
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-05-03 21:05:23 +0300

    favourite button added to info page

--- Main_js.js
+++ Main_js.js
@@ -27,6 +27,7 @@ const next = $(`#next`);
 const previous = $(`#previous`);
 const last= $(`#last`);
 const first = $(`#first`);
+const add_fav = $(`#favouriteButton`);
 
 
 // cycling between pages
@@ -307,6 +308,7 @@ $(`#changeColor`).on(`click`, () => {
         })
         $(`body`).css(`background-color`, `#14213d`);
         $(`.colorScheme1`).css(`color`, `#000000`);
+        $(`#ani_title`).css(`color`, `#000000`);
         $(`.colorScheme2`).css(`border`, `solid #FFFFFF 2px`);
         $(`.colorScheme2a`).css(`border-bottom`, `solid #FFFFFF 2px`);
         $(`.confirm`).on(`mouseover`,() => {
@@ -336,6 +338,7 @@ $(`#changeColor`).on(`click`, () => {
         })
         $(`body`).css(`background-color`, `#f1faee`);
         $(`.colorScheme1`).css(`color`, `#457b9d`);
+        $(`#ani_title`).css(`color`, `#ffffff`);
         $(`.colorScheme2`).css(`border`, `solid #e63946 2px`);
         $(`.colorScheme2a`).css(`border-bottom`, `solid #e63946 2px`);
         $(`.confirm`).css(`color`, `#a8dadc`);
@@ -409,11 +412,27 @@ explore.on(`click`, () => {
             previous.unbind(`click`);
             first.unbind(`click`);
             last.click(() => {pageCycle(data.links.last)});            
-            next.click(() => {pageCycle(data.links.next)})          
-            previous.click(() => {pageCycle(data.links.prev)})            
-            first.click(() => {pageCycle(data.links.first)})
+            next.click(() => {pageCycle(data.links.next)});          
+            previous.click(() => {pageCycle(data.links.prev)});            
+            first.click(() => {pageCycle(data.links.first)});
         }
     )
 })
 
-
+// favourite button hover
+add_fav.on(`mouseover`, () => {
+    add_fav.empty()
+    add_fav.css(`background-color`, `#a8dadc`)
+    add_fav.css(`color`, `#1d3557`)
+    add_fav.append(`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+  </svg> favourite`)
+})
+add_fav.on(`mouseout`, () => {
+    add_fav.empty()
+    add_fav.css(`background-color`, `#1d3557`)
+    add_fav.css(`color`, `#a8dadc`)
+    add_fav.append(`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+    <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+  </svg> favourite`)
+})
